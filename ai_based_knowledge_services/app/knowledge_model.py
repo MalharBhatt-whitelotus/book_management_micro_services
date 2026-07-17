0cb73355d92b009bd565dd004e1e6446ba44de88
Malhar Bhatt <malhar@whitelotuscorporation.com>
2026-07-17 12:07:35 +0530

    Add PDF Details table.

--- ai_based_knowledge_services/app/knowledge_model.py
+++ ai_based_knowledge_services/app/knowledge_model.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String, Integer, DateTime
+from sqlalchemy import Column, String, Integer, DateTime, Text
 
 from ai_based_knowledge_services.app.knowledge_database import Base
 
@@ -10,3 +10,18 @@ class KnowledgeModel(Base):
     content_type = Column(String, default="pdf")
     uploaded_at = Column(DateTime, nullable=False, index=True)
     owner_id = Column(Integer, nullable=False)
+
+class PdfDetails(Base):
+    __tablename__ = "pdf_details"
+    id= Column(Integer, primary_key=True)
+    filename = Column(String, nullable=False, index=True)
+    stored_filename = Column(String)
+    filepath = Column(String)
+    file_size = Column(Integer)
+    page_count = Column(Integer)
+    title = Column(String)
+    author = Column(String)
+    creator = Column(String)
+    producer = Column(String)
+    content = Column(Text)
+    uploaded_at = Column(DateTime)
--- ai_based_knowledge_services/app/knowledge_schema.py
+++ ai_based_knowledge_services/app/knowledge_schema.py
@@ -1,11 +1,43 @@
 from pydantic import Field
-from datetime import datetime
 from ai_based_knowledge_services.app.knowledge_model import KnowledgeModel
 
 class KnowledgeRequest:
-    id: int = Field(...)
     filename: str = Field(..., min_length=1, max_length=100)
     filepath: str = Field(..., min_length=1, max_length=100)
     content_type: str = Field(..., default="pdf", min_length=1,max_length=10)
-    uploaded_at: datetime = Field(...)
+    uploaded_at: str = Field(...)
     ower_id: int = Field(...)
+
+class KnowledgeResponse:
+    id: int 
+    filename: str
+    filepath: str
+    content_type: str
+    uploaded_at: str
+    ower_id: int
+
+class PdfUploads:
+    filename : str = Field(..., min_length=1, max_length=100)
+    stored_filename: str
+    filepath: str = Field(...)
+    file_size: int = Field(...)
+    page_count: int = Field(...)
+    title: str = Field(..., min_length=1, max_length=100)
+    author: str = Field(..., min_length=1, max_length=100)
+    creator: str
+    producer: str
+    content: str = Field(...)
+    uploaded_at: str = Field(...)
+class PdfUploads:
+    id: int
+    filename : str
+    stored_filename: str
+    filepath: str
+    file_size: int
+    page_count: int
+    title: str
+    author: str
+    creator: str
+    producer: str
+    content: str
+    uploaded_at: str
