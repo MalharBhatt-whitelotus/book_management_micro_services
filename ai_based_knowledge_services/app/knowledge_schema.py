79d805808416b22f0ee502d111bd1ebbad9b426b
Malhar Bhatt <malhar@whitelotuscorporation.com>
2026-07-17 12:20:46 +0530

    Resolve the PDF Tables

--- ai_based_knowledge_services/app/knowledge_schema.py
+++ ai_based_knowledge_services/app/knowledge_schema.py
@@ -1,21 +1,6 @@
 from pydantic import Field
 from ai_based_knowledge_services.app.knowledge_model import KnowledgeModel
 
-class KnowledgeRequest:
-    filename: str = Field(..., min_length=1, max_length=100)
-    filepath: str = Field(..., min_length=1, max_length=100)
-    content_type: str = Field(..., default="pdf", min_length=1,max_length=10)
-    uploaded_at: str = Field(...)
-    ower_id: int = Field(...)
-
-class KnowledgeResponse:
-    id: int 
-    filename: str
-    filepath: str
-    content_type: str
-    uploaded_at: str
-    ower_id: int
-
 class PdfUploads:
     filename : str = Field(..., min_length=1, max_length=100)
     stored_filename: str
