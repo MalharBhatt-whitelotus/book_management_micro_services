9ad43bac34b0a3bd9c30360dd6814c981acd4811
Malhar Bhatt <malhar@whitelotuscorporation.com>
2026-07-20 16:46:35 +0530

    optimize the book client

--- bill_services/app/service/book_client.py
+++ bill_services/app/service/book_client.py
@@ -7,8 +7,8 @@ class BookClient:
     async def get_book_by_id(book_id: int):
         async with httpx.AsyncClient() as client:
             response = await client.get(f"{BookClient.API_URL}/get_book_by_id/{book_id}")
-            print(response.status_code)
-            print(response.json())
+            # print(response.status_code)
+            # print(response.json())
             if response.status_code != 200:
                 raise HTTPException(status_code=404,detail="BOOK NOT FOUND.")
             return response.json()
@@ -17,14 +17,12 @@ class BookClient:
     async def reduce_book_stock(book_id: int, quantity: int):
         async with httpx.AsyncClient() as client:
             response = await client.patch(f"{BookClient.API_URL}/reduce_book_stock",params={"book_id":book_id,"quantity":quantity})
-            print(response.request.method)
-            print(response.request.url)
-            print(response.status_code)
-            print(response.headers)
-            print(response.text)    
-            print(response.json())
-            if response.status_code != 200:
-                raise HTTPException(status_code=409,detail="Books are not reduced.")
+            # print(response.request.method)
+            # print(response.request.url)
+            # print(response.status_code)
+            # print(response.headers)
+            # print(response.text)    
+            # print(response.json())
+            # if response.status_code != 200:
+            #     raise HTTPException(status_code=409,detail="Books are not reduced.")
             return response.json()
-        
-    
